324c9aa69adf527849e6ba1310a9b57148b2483c
n3al <programingn3al@gmail.com>
2025-04-19 23:46:31 -0500

    Limpiar CSS viejo y actualizar layout del dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,30 +10,47 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('css/dashboard-user.css') }}">
-
+        <link rel="stylesheet" href="{{ asset('css/dashboard-user.css') }}?v={{ time() }}">
+        <style>
+            .dashboard-background {
+                background-image: url('{{ asset("img/1401.jpg") }}');
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+                position: relative;
+            }
+            .dashboard-background::before {
+                content: '';
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background: rgba(15, 23, 42, 0.85);
+            }
+        </style>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body id="bg-dashboard-user" class="font-sans antialiased">
-       
-        <div id="app-container" class="min-h-screen bg-slate-800 dark:bg-gray-900">
+    <body class="font-sans antialiased">
+        <div id="app-container" class="min-h-screen dashboard-background">
+            <div class="relative z-10">
+                @include('layouts.navigation')
 
-            @include('layouts.navigation')
+                <!-- Page Heading -->
+                @isset($header)
+                    <header class="shadow">
+                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                            {{ $header }}
+                        </div>
+                    </header>
+                @endisset
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header id="page-header" class="bg-black dark:bg-gray-800 shadow">
-                    <div id="header-container" class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main id="main-content">
-                {{ $slot }}
-            </main>
+                <!-- Page Content -->
+                <main id="main-content">
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
     </body>
 </html>
